218f2855280767a8b1b665b8bfadd45c279ffe26
Thierry Lanfranchi <thierryantispam+github@gmail.com>
2019-09-25 11:05:34 +0200

    Added GET products route and tests

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,17 @@
 # wsgi.py
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+PRODUCTS = [
+    { 'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' },
+    { 'id': 3, 'name': 'IDontKnow' }
+]
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/api/v1/products')
+def get_products():
+    return jsonify(PRODUCTS)
